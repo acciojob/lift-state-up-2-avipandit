33916b2140e863a45bccfc3d94fabf9180ff220c
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-05-29 12:58:13 +0530

    Update App.js

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import './../styles/App.css';
+
+const App = () => {
+  const[data,setData]=useState("");
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <p>{data}</p>
+     <div className="child">
+      <h1>Child Component</h1>
+      <input 
+      type="text"
+      value={data}
+      onChange={(e)=>setData(e.target.value)}
+      />
+     </div>
+    </div>
+  )
+}
+
+export default App
